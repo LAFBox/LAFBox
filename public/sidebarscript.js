6205416c89b040df5f7f888abcd55792cc971306
SCHSLostAndFound <zdjohannes@gmail.com>
2025-12-13 02:18:57 -0600

    extensive change to work with firebase

--- public/sidebarscript.js
+++ public/sidebarscript.js
@@ -2,10 +2,9 @@
 
 //Function to make the Sidebar visible. Puts the sidebar element into a variable. In the CSS the sidebar is autoamtically transformed off the page. When this function is called, it brings the function back into a visible area.
 function showSidebar(){
-			const sidebar = document.querySelector('.sidebar')
-			sidebar.style.transform = 'translateX(0%)'
-			
-		}
+	const sidebar = document.querySelector('.sidebar')
+	sidebar.style.transform = 'translateX(0%)'		
+}
 //Function to make the sidebar invisible. It transforms the sidebar out of view of the screen. 
 function closeSidebar(){
 	const sidebar = document.querySelector('.sidebar')
